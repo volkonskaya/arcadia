e4c54ad403bd54f2c24bac9b185dbfba7c6e6ea9
Tatiana Smirnova <tsmirnova.me@gmail.com>
2021-03-23 19:34:42 +0300

    task 2 and 3 added to lesson 2

---
+++ Lesson 2/Task1_Auth/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task1_Auth
+{
+    class Program
+    {
+        private static object userUsername;
+        private static object userPassword;
+        public static void Credentials()
+        {
+            Console.WriteLine("Enter a username: ");
+            userUsername = Console.ReadLine();
+            Console.WriteLine("Enter a password: ");
+            userPassword = Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your credentials");
+
+            string username = "tania";
+            string password = "12345";
+            const int maxAttempt = 2;
+            int attemp = 0;
+            Credentials();
+
+            if (username.Equals(userUsername) && password.Equals(userPassword))
+            {
+                Console.WriteLine($"Welcome {username}! Nice to see you again!");
+            }
+            else
+            {
+                while (attemp < maxAttempt)
+                {
+                    attemp++;
+                    Console.WriteLine("Wrong username or password, please try again");
+                    Credentials();
+                }
+                Console.WriteLine("Error! The maximum number of attempts has been exhausted!");
+            }
+        }
+    }
+}
---
+++ Lesson 2/Task2_Calculator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Task2_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int operand1, operand2;
+            double result;
+            Console.WriteLine("Enter 1st operand: ");
+            operand1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter symbol. +-/*^ are allowed: ");
+            string symbol = Console.ReadLine();
+            Console.WriteLine("Enter 2nd operand: ");
+            operand2 = Convert.ToInt32(Console.ReadLine());
+            if (symbol == "+")
+            {
+                result = operand1 + operand2;
+                Console.WriteLine($"Your result: {operand1} + {operand2} = {result}");
+            }
+
+            if (symbol == "-")
+            {
+                result = operand1 - operand2;
+                Console.WriteLine($"Your result: {operand1} - {operand2} = {result}");
+            }
+
+            if (symbol == "*")
+            {
+                result = operand1 * operand2;
+                Console.WriteLine($"Your result: {operand1} * {operand2} = {result}");
+            }
+
+            if (symbol == "/")
+            {
+                while (operand2 == 0)
+                {
+                    Console.WriteLine("Enter a non-zero divisor: ");
+                    operand2 = Convert.ToInt32(Console.ReadLine());
+                }
+                result = operand1 / operand2;
+                Console.WriteLine($"Your result: {operand1} / {operand2} = {result}");      
+            }
+
+            if (symbol == "^")
+            {
+                result = (int)Math.Pow(operand1, operand2);
+                Console.WriteLine($"Your result: {operand1} ^ {operand2} = {result}");
+            }
+
+        }
+    }
+}
