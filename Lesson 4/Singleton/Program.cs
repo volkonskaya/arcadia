a00580a52cef7365780073f280de13fd16cdf3bd
Tatiana Smirnova <tsmirnova.me@gmail.com>
2021-04-12 21:27:38 +0300

    lesson 4 - singleton was added

---
+++ Lesson 4/Singleton/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Single
+    {
+        private static Single instance;
+
+        private Single() //constructor
+        { }
+
+        public static Single getinstance()
+        {
+            if (instance == null)
+                instance = new Single();
+            return instance;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Single s1 = Single.getinstance();
+            Single s2 = Single.getinstance();
+
+            if (s1.GetHashCode() == s2.GetHashCode())
+            {
+                Console.WriteLine("HashCode of s1 = HashCode of s2");
+            }
+            else
+            {
+                Console.WriteLine("HashCode of s1 != HashCode of s2");
+            }
+        }
+    }
+}
